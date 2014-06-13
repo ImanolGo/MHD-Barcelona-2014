f33fe051f0845a670c952d1563d470f7b6694c2f
Alvaro Sarasua <alvarosarasua@gmail.com>
2014-06-13 12:54:58 +0200

    Final freesound

--- py/getSoundsFromText.py
+++ py/getSoundsFromText.py
@@ -4,18 +4,22 @@ import subprocess
 c = freesound.FreesoundClient()
 c.set_token("36e286ba563287f86eed608c13cacd520ced010e","token")
 
-text_file = open('3pigs.txt', 'r')
+text_file = open('../data/text/sonar_knights.txt', 'r')
 
 words = text_file.read().split()
 sounds = {}
 
+i = 0
 for word in words:
 	if word not in sounds.keys() and '.' in word:
 		word = word.replace('.', '')
 		print "Searching for " + word + ":"
 		print "----------------------------"
-		results_pager = c.text_search(query=word, filter="duration:[0 TO 1.5]" , sort="rating_desc", fields="id,name,previews")
-		sounds[word] = results_pager[0].retrieve_preview('.', word+'.mp3')
-		convertedfile = word+'.mp3'
-		cmd = 'lame --resample 44.1 %s' % convertedfile
-		subprocess.call(cmd, shell=True)
+		results_pager = c.text_search(query=word, filter="duration:[0.5 TO 3]" , sort="downloads_desc", fields="id,name,previews")
+		if len(results_pager.results) > 0:
+			sounds[word] = results_pager[0].retrieve_preview('.', word+'.mp3')
+			convertedfile = word+'.mp3'
+			outputfilename = '../data/audio/' + str(i)+'.mp3'
+			cmd = 'lame --resample 44.1 ' + convertedfile + ' ' + outputfilename
+			subprocess.call(cmd, shell=True)
+			i = i+1
